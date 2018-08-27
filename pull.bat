ea009eef9ef6fab8490dac16501b030cb307897c
cfuster-fundaciobit <cfuster@fundaciobit.org>
2018-08-27 14:23:09 +0200

    Canviar noms dels plugins v 2.0 #59

---
+++ pull.bat
@@ -0,0 +1,18 @@
+call git -C ../pluginsib-core pull
+call git -C ../pluginsib-utils pull
+call git -C ../pluginsib-userinformation pull
+call git -C ../pluginsib-barcode pull
+call git -C ../pluginsib-exportdata pull
+call git -C ../pluginsib-validatecertificate pull
+call git -C ../pluginsib-csvgenerator pull
+call git -C ../pluginsib-documentconverter pull
+call git -C ../pluginsib-loginmodulejboss pull
+call git -C ../pluginsib-validatesignature pull
+call git -C ../pluginsib-paymentweb pull
+call git -C ../pluginsib-timestamp pull
+call git -C ../pluginsib-documentcustody pull
+call git -C ../pluginsib-scanweb pull
+call git -C ../pluginsib-arxiu pull
+
+
+echo  ===============================  A L L  P U L L E D =====================================
---
+++ pull.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+git -C ../pluginsib-core pull
+git -C ../pluginsib-utils pull
+git -C ../pluginsib-userinformation pull
+git -C ../pluginsib-barcode pull
+git -C ../pluginsib-exportdata pull
+git -C ../pluginsib-validatecertificate pull
+git -C ../pluginsib-csvgenerator pull
+git -C ../pluginsib-documentconverter pull
+git -C ../pluginsib-loginmodulejboss pull
+git -C ../pluginsib-validatesignature pull
+git -C ../pluginsib-paymentweb pull
+git -C ../pluginsib-timestamp pull
+git -C ../pluginsib-documentcustody pull
+git -C ../pluginsib-scanweb pull
+git -C ../pluginsib-arxiu pull
+
+
+echo  ===============================  A L L  P U L L E D =====================================
